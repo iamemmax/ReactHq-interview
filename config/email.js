ca5e7df35a1d0e1d2c7d337a907e5157aeb3d2fa
emmax <iam4emmax@gmail.com>
2022-05-23 13:05:11 +0100

    fixed submitting error

--- config/email.js
+++ config/email.js
@@ -1,35 +1,30 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
-module.exports =  async(email, subject, html) =>{
-
-
-
-let transporter = await nodemailer.createTransport({
-    service: 'gmail',
-     host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+module.exports = async (email, subject, html) => {
+  let transporter = await nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-        user: process.env.email,
-        pass: process.env.password
-       }
-     });
-     
-     var mailOptions = {
-       from: `ReactHq "no-reply@reactHq.com"`,
-       replyTo: 'no-reply@reactHq.com',
-       to: email,
-       subject: subject,
-       html: html
-     };
-     
-     await transporter.sendMail(mailOptions, function(error, info){
-       if (error) {
-         console.log(error);
-       } else {
-         console.log('Email sent: ' + info.response);
-       }
-     });
+      user: process.env.email,
+      pass: process.env.password,
+    },
+  });
 
+  var mailOptions = {
+    from: `ReactHq "no-reply@reactHq.com"`,
+    replyTo: "no-reply@reactHq.com",
+    to: email,
+    subject: subject,
+    html: html,
+  };
 
-}
+  await transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
