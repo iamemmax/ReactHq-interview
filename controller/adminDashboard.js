87dcb4b5e739b581b309e94b685bf834892a45dc
emmax <iam4emmax@gmail.com>
2022-05-27 07:18:51 +0100

    added edit user on admin dashboard

--- controller/adminDashboard.js
+++ controller/adminDashboard.js
@@ -24,7 +24,99 @@ exports.generateUserPdf = asyncHandler(async (req, res) => {
     (err, html) => {
       pdf
         .create(html, option)
-        .toFile(`assets/docs/users/${Date.now()}`, (err, data) => {});
+        .toFile(`assets/docs/users/${Date.now()}`, (err, data) => {
+          if (err) console.log(err);
+          if (data) {
+            let dataFile = fs.readFileSync(`assets/docs/users/${Date.now()}`);
+            res.header("content-type", "application/pdf");
+            res.send(dataFile);
+          }
+        });
     }
   );
 });
+
+exports.editUser = asyncHandler(async (req, res) => {
+  let error = [];
+  let user = await userSchema.findById(req.params.id);
+  if (!user) {
+    error.push({ msg: "user not found" });
+    res.render("./admin/dashboard/users", {
+      layout: "./layouts/dashboardLayouts",
+      users,
+    });
+    return;
+  }
+  res.render("./admin/dashboard/editUser", {
+    layout: "./layouts/dashboardLayouts",
+    user,
+  });
+});
+
+//@DESC:update user
+exports.updateUser = async (req, res) => {
+  let error = [];
+
+  let { firstName, lastName, email, phone, acct_No, course } = req.body;
+
+  //  @desc check if user fill all fields
+  if (!firstName || !lastName || !email || !phone || !acct_No || !course) {
+    error.push({ msg: "all field are required" });
+
+    return;
+  }
+
+  // @desc check if user enter valid email
+  function validateEmail(email) {
+    const regex =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+  }
+  if (!validateEmail(email)) {
+    error.push({ msg: "please enter a valid email" });
+
+    return;
+  }
+  let user = await userSchema.findById(req.params.id);
+
+  if (error.length > 0) {
+    try {
+      await userSchema.findByIdAndUpdate(
+        { _id: { $eq: req.params.id } },
+        {
+          $set: {
+            firstName: firstName || user.firstName,
+            lastName: lastName || user.lastName,
+            email: email || user.email,
+            phone: phone || user.phone,
+            acct_No: acct_No || user.acct_No,
+            course: course || user.course,
+          },
+        },
+        { new: true },
+        (err, data) => {
+          if (err) {
+            error.push({ msg: "unable to update user" });
+            res.render("./admin/dashboard/editUser", {
+              layout: "./layouts/dashboardLayouts",
+              user,
+              error,
+            });
+          }
+          if (data) {
+            res.redirect("/admin/users");
+          }
+        }
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  } else {
+    error.push({ msg: "unable to update user" });
+    res.render("./admin/dashboard/editUser", {
+      layout: "./layouts/dashboardLayouts",
+      user,
+      error,
+    });
+  }
+};
--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUser, generateUserPdf } = require("../controller/adminDashboard");
+const {
+  getAllUser,
+  generateUserPdf,
+  editUser,
+} = require("../controller/adminDashboard");
 router.route("/users", getAllUser).post(generateUserPdf);
+router.route("/users/edit:id").get(editUser).put(updateUser);
 module.exports = router;
